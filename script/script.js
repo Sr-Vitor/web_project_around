fa5efa5bdc518a70faa124ebee30b04852e3d097
Sr-Vitor <vitor_augusto1994@icloud.com>
2024-12-26 19:41:11 +0100

    continuação do projeto

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     formSelector: ".popup__container",
     inputSelector: ".popup__input",
     submitButtonSelector: ".popup__submit",
-    inactiveButtonClass: "popup__button_disabled",
+    inactiveButtonClass: "popup__submit_disabled",
     inputErrorClass: "popup__input_type_error",
     errorClass: "popup__error_visible",
   };
