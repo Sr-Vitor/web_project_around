8ddf219ec403dd529545eb0fe78bdf62305e6853
Sr-Vitor <vitor_augusto1994@icloud.com>
2025-05-01 07:34:04 +0200

    configuração da pasta

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/script/index.js", // ponto de entrada do JS
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true, // limpa dist a cada build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"], // para importar CSS no JS
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // copia e insere <script> automaticamente
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    open: true,
+  },
+  mode: "development",
+};
